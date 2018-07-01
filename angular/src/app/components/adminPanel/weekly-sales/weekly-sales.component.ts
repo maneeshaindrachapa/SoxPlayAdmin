c96950f396acab334fe574e1cf53485f85287a16
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-07-01 15:23:26 +0530

    weekley sales component added

---
+++ angular/src/app/components/adminPanel/weekly-sales/weekly-sales.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-weekly-sales',
+  templateUrl: './weekly-sales.component.html',
+  styleUrls: ['./weekly-sales.component.css']
+})
+export class WeeklySalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
