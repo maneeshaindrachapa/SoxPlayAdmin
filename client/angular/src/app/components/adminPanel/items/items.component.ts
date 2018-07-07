78af1f24eb659e2adcd8e4aed9d9e9272340ce47
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-07-08 00:30:33 +0530

    items Display Added

--- client/angular/src/app/components/adminPanel/items/items.component.ts
+++ client/angular/src/app/components/adminPanel/items/items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../../../services/sdk/models";
+import { ItemApi,ThemeApi } from '../../../services/sdk/services/custom';
 
 @Component({
   selector: 'app-items',
@@ -20,17 +22,50 @@ export class ItemsComponent implements OnInit {
 
   themeList = [];
 
-  //item = new Item();
+  item = new Item();
 
   selected = [];
-  constructor() { }
+  constructor(private itemApi:ItemApi,private themeApi:ThemeApi) { 
+
+  }
 
   ngOnInit() {
+    this.themeApi.find().subscribe(res => {
+      this.themeList = res;
+    });
+    this.itemApi.find({include: 'theme'}).subscribe(res => {
+      console.log(res);
+      this.rows = res;
+    });
   }
   onSale(row) {
     console.log(row);
   }
+
+  addItem() {
+    this.itemApi.create(this.item).subscribe(res => {
+      this.rows.push(res);
+      this.rows = [...this.rows];
+    });
+  }
+
+  editItem() {
+    this.itemApi.upsert(this.item).subscribe(res => {
+      this.selected[0] = res;
+      this.rows = [...this.rows];
+      this.selected = [];
+    });
+  }
+
+  deleteItem() {
+    if (confirm('Are you sure to delete this items ?')) {
+      this.itemApi.deleteById(this.item.id).subscribe(res => {
+        this.ngOnInit();
+      });
+    }
+  }
+
   selectItem(i) {
-    //this.item = i.selected[0];
+    this.item = i.selected[0];
   }
 }
