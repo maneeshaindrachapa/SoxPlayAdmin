8c8913093c35c8fb617fce037191ba92459a666e
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-07-07 09:32:41 +0530

    app module updated

--- client/angular/src/app/app.module.ts
+++ client/angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './components/signin/signin/signin.component';
@@ -9,9 +10,12 @@ import { NavbarComponent } from './components/adminPanel/navbar/navbar.component
 import { WeeklySalesComponent } from './components/adminPanel/weekly-sales/weekly-sales.component';
 import { DashboardComponent } from './components/adminPanel/dashboard/dashboard.component';
 import {ToggleService}  from './services/toggle.service';
+import { AddItemComponent } from './components/adminPanel/add-item/add-item.component';
+import { ItemsComponent } from './components/adminPanel/items/items.component';
+import { ItemsEditDeleteComponent } from './components/adminPanel/items-edit-delete/items-edit-delete.component';
 
 const appRoutes: Routes= [
-  {path:"", component:SigninComponent},{path:"dashboard",component:DashboardComponent}
+  {path:"", component:SigninComponent},{path:"dashboard",component:DashboardComponent},{path:"items",component:ItemsEditDeleteComponent}
 ];
 
 @NgModule({
@@ -20,9 +24,13 @@ const appRoutes: Routes= [
     SigninComponent,
     NavbarComponent,
     WeeklySalesComponent,
-    DashboardComponent
+    DashboardComponent,
+    AddItemComponent,
+    ItemsComponent,
+    ItemsEditDeleteComponent
   ],
   imports: [
+    NgxDatatableModule,
     BrowserModule,
     AngularFontAwesomeModule,
     RouterModule.forRoot(appRoutes)
