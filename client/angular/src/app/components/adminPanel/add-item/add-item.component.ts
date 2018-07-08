39757639582f2fc9c477f4ac71f225087ea8c3ec
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-07-08 16:21:03 +0530

    UI fix

--- client/angular/src/app/components/adminPanel/add-item/add-item.component.ts
+++ client/angular/src/app/components/adminPanel/add-item/add-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../../../services/sdk/models";
+import { ItemApi,ThemeApi } from '../../../services/sdk/services/custom';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-add-item',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
+  item = new Item();
+  themeList = [];
 
-  constructor() { }
+  constructor(private itemApi:ItemApi,private themeApi:ThemeApi) {
+    
+   }
 
   ngOnInit() {
+    this.themeApi.find().subscribe(res => {
+      this.themeList = res;
+    });  
   }
-
+  
+  addItem() {
+    this.itemApi.create(this.item).subscribe(res => {
+      Swal("Item Added!", "You Item Added Successfully", "success");
+    });
+  }
 }
