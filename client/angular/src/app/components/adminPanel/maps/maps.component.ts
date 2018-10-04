3c3eac6a7f305fc081f273db63004849190d5e26
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2018-10-04 09:07:49 +0530

    maps functionality added

--- client/angular/src/app/components/adminPanel/maps/maps.component.ts
+++ client/angular/src/app/components/adminPanel/maps/maps.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
 import { StoresApi } from '../../../services/sdk/services/custom';
+import { Stores } from '../../../services/sdk/models/Stores';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-maps',
@@ -8,25 +9,26 @@ import { StoresApi } from '../../../services/sdk/services/custom';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  totalStores=[];
-  latitudes=[];
-  longitudes=[];
+  totalStores = [];
+  store = new Stores();
   lng: number = 10;
   lat: number = 45.568;
 
-  constructor(private storeApi:StoresApi) { }
+  constructor(private storeApi: StoresApi) { }
 
   ngOnInit() {
     this.getStoreDetails();
   }
-  
-  getStoreDetails(){
-    this.storeApi.find().subscribe(data=>{
-      this.totalStores=data;
-      for(let i=0;i<this.totalStores.length;i++){
-        this.latitudes.push(this.totalStores[i]["lat"]);
-        this.longitudes.push(this.totalStores[i]["lng"]);
-      }
+
+  getStoreDetails() {
+    this.storeApi.find().subscribe(data => {
+      this.totalStores = data;
     })
+  }
+  addStore() {
+    this.storeApi.create(this.store).subscribe(res => {
+      Swal("Branch Added!", "You Branch Added  to Map Successfully", "success");
+      this.getStoreDetails();
+    });
   }
 }
